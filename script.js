18d80baed7ed4fc67e4ba8b69ef2ab1b61f1a96e
Erica <ericac2700@tuckahoeschools.org>
2024-11-06 13:31:24 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 //Use query Selectors to get the class of all the button shapes
+const redLight = document.querySelector('.red-light');
+const yellowLight = document.querySelector('.yellow-light');
+const greenLight = document.querySelector('.green-light');
 
 //Use getElementById which allows us to get the ID so that we can add an event listener to the buttons, listen for a click and then set the color via css properties.
+const redSwitch = document.getElementById('red-switch');
+const yellowSwitch = document.getElementById('yellow-switch');
+const greenSwitch = document.getElementById('green-switch');
 
 //Add event listeners that listen for a button click
+redSwitch.addEventListener('click', () =>{
+    redLight.style.backgroundColor = 'red';
+});
+yellowSwitch.addEventListener('click', () =>{
+    yellowLight.style.backgroundColor = 'yellow';
+});
+greenSwitch.addEventListener('click', () =>{
+    greenLight.style.backgroundColor = 'green';
+});
+
 
 //Turn all lights on and off with their specific buttons
+
 
 //Party Mode Button
 
