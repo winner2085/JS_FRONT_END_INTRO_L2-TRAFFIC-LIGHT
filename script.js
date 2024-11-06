61f03dd3621213706d8574b4975d4f6bb7ef0193
Cooper <104042225+thanksfortheride136@users.noreply.github.com>
2024-11-06 03:50:35 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Use query Selectors to get the class of all the button shapes
+
+//Use getElementById which allows us to get the ID so that we can add an event listener to the buttons, listen for a click and then set the color via css properties.
+
+//Add event listeners that listen for a button click
+
+//Turn all lights on and off with their specific buttons
+
+//Party Mode Button
+
+//Turn all lights on and off at once with a single button
